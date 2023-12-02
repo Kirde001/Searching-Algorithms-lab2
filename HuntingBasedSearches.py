260968069b6a02d99447f2fe126a219091ab33c5
Kirill Poliakov <145561178+Kirde001@users.noreply.github.com>
2023-12-02 23:43:28 +0300

    Update HuntingBasedSearches.py

--- HuntingBasedSearches.py
+++ HuntingBasedSearches.py
@@ -50,6 +50,78 @@ def hunting_search_Kirde(list,key):
         except IndexError:
             flag = False
     return count
+#######################################################################
+// a better version of that one higher, actually I've just removed unnecessary things and etc
+def hunting_search_Kirde_mod(list,key):
+    high = max = len(list)-1 
+    low = n = i = 0
+    count = 0 
+    flag = True 
+    while high > low:
+        try:
+            n +=1  # придется пихать в места внутренних вайлов и в ветвление
+            i = 0
+            if list[high] == key:
+                count +=1 
+                list.pop(high) 
+                high = len(list)-1 # какая граница работает, такая и перезадается
+            while key < list[high]: 
+                high -= 2**i 
+                i += 1 
+                if high-2**i <= low: 
+                    i = 0
+                if list[high] == key: 
+                    count +=1
+                    list.pop(high)
+                    high = len(list)-1
+            else: 
+                low = list.index(list[high])
+            i = 0
+            while key > list[low]:
+                low += 2**i
+                i += 1
+                if high <= low + 2**i:  
+                    i = 0
+                if key == list[low]:
+                    count +=1
+                    list.pop(low)
+                    low = 0
+            else:
+                high = list.index(list[low])
+        except IndexError:
+            flag = False
+    return count,n 
+########################################################################################
+// this is another variety of hunter search made by me, idk about it's perfomance but we've got here less lines of code
+def girl_hunter_ver2(list,key):
+    low = count = i = 0 
+    gran = len(list)-1
+    pov = False
+    while key in list:
+        try:
+            if list[gran] == key:
+                list.pop(gran)
+                count += 1 
+                gran = len(list)-1
+            while (list[gran] > key and pov == False) or (list[gran] < key and pov == True):
+                if pov == True:
+                    gran = -gran
+                if list[gran] == key:
+                    count += 1
+                    list.pop(gran)
+                    gran = len(list)-1
+                else:
+                    i += 1 
+                    if gran-2**i < low:
+                        i = 0
+                    gran = abs(gran-2**i)
+                    i += 1 
+            else:
+                pov = not pov
+        except IndexError:
+            pass
+    return count
+###########################################################################################################
 def hunting_binary_search_Kirde(list,key):
     high = len(list)-1
     low = n = count = 0
